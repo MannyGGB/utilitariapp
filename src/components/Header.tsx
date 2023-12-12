c8577597c238fcbe7b24f03ac019b40173cebc4d
Manuel <manugonzalez.tech@gmail.com>
2023-12-12 16:25:49 +0000

    Add homepage app logos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+
 export default function Header() {
   return (
     <>
       <h1>Utilitariapp</h1>
-      <nav>
-        <Link href={"./"}>Home</Link>
-        <Link href={"./puttogether"}>Put Together</Link>
+      <nav className="flex justify-evenly">
+        <Link href={"./"}>
+          <Image src="/toolbox.png" alt="homepage" width="97" height="40" />
+        </Link>
+        <Link href={"./puttogether"}>
+          <Image
+            src="/problem-solving.png"
+            alt="put together app"
+            width="97"
+            height="40"
+          />
+        </Link>
       </nav>
     </>
   );
